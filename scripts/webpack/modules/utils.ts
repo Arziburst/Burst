c32945a257044c850059b6e70c011266a3f4de8d
Eugene Mormul <zhenya.mormul@gmail.com>
2021-09-24 14:46:40 +0300

    upd

--- scripts/webpack/modules/utils.ts
+++ scripts/webpack/modules/utils.ts
@@ -9,12 +9,12 @@ import WebpackBar from 'webpackbar';
 import FriendlyErrorsWebpackPlugin from 'friendly-errors-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { GenerateSW } from 'workbox-webpack-plugin';
-//import WebpackPwaManifest from 'webpack-pwa-manifest';
+import WebpackPwaManifest from 'webpack-pwa-manifest';
 import dotenv from 'dotenv';
-//import { resolve } from 'path';
+import { resolve } from 'path';
 
 // Constants
-//import { SOURCE_DIRECTORY } from '../constants';
+import { SOURCE_DIRECTORY } from '../constants';
 
 export const connectBuildProgressIndicator = (): Configuration => ({
     plugins: [ new WebpackBar({ basic: true }) ],
@@ -81,25 +81,25 @@ export const generateServiceWorker = (): Configuration => {
     };
 };
 
-// export const generateManifest = (): Configuration => {
-//     const manifest = new WebpackPwaManifest({
-//         name:             process.env.APP_NAME || 'Awesome app',
-//         short_name:       process.env.APP_NAME || 'Awesome app',
-//         background_color: '#ffffff',
-//         crossorigin:      'use-credentials',
-//         display:          'standalone',
-//         inject:           true,
-//         ios:              true,
-//         icons:            [
-//             {
-//                 src:   resolve(SOURCE_DIRECTORY, './assets/images/logo.png'),
-//                 sizes: [ 96, 128, 192, 256, 384, 512 ],
-//                 ios:   true,
-//             },
-//         ],
-//     });
+export const generateManifest = (): Configuration => {
+    const manifest: WebpackPwaManifest = new WebpackPwaManifest({
+        name:             process.env.APP_NAME || 'Awesome app',
+        short_name:       process.env.APP_NAME || 'Awesome app',
+        background_color: '#ffffff',
+        crossorigin:      'use-credentials',
+        display:          'standalone',
+        inject:           true,
+        ios:              true,
+        icons:            [
+            {
+                src:   resolve(SOURCE_DIRECTORY, './assets/images/logo.png'),
+                sizes: [ 96, 128, 192, 256, 384, 512 ],
+                ios:   true,
+            },
+        ],
+    });
 
-//     return {
-//         plugins: [ manifest ],
-//     };
-// };
+    return {
+        plugins: [ manifest ],
+    };
+};
