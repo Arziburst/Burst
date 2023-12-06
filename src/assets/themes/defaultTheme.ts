1d8c3880445db3f5db30ac7e8f97ff37d7a6780e
Dmytro Holub <dmitriygolubapk@gmail.com>
2023-12-06 20:52:54 +0300

    {theme} arument in styled component typed

--- src/assets/themes/defaultTheme.ts
+++ src/assets/themes/defaultTheme.ts
@@ -1,8 +1,35 @@
-export const defaultTheme = {
+// Colors
+import { green } from '../colors/green';
+
+export interface ITheme {
+    name: string
+    button: {
+        primary: string
+        secondary: string
+        error: string
+    }
+    palette: {
+        primary?: Partial<typeof green>
+        secondary?: Partial<typeof green>
+        background?: {
+            default: string
+        }
+    },
+}
+
+export const defaultTheme: ITheme = {
     name:   'defaultTheme',
     button: {
         primary:   '#000',
         secondary: '#fff',
         error:     '#FF0000',
+    },
+    palette: {
+        primary: {
+            ...green,
+        },
+        background: {
+            default: '#FFFFFF',
+        },
     },
 };
